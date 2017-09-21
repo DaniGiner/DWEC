bb17495702f5c01231ee4614bfb5bc87289907cb
Dani <daniginervillalba@gmail.com>
2017-09-21 20:44:22 +0200

    U1_P2

--- UD1_P2/U2Ej3script.js
+++ UD1_P2/U2Ej3script.js
@@ -1,1 +1,1 @@
-var t = ("Las rosas son rojas.<br>&nbsp; &nbsp; &nbsp; &nbsp;El mar es azul.<br> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; No sé rimar");
+var t = ("Las rosas son rojas.<br> &nbsp; &nbsp; &nbsp; &nbsp;El mar es azul.<br> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; No sé rimar");
